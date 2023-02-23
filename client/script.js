a7806533601e33a0123baffa0a609a194e75b2c9
roope <roope.tahvanainen@gmail.com>
2023-02-23 10:41:05 +0200

    url kuntoon vol 2

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
     // 'https://codex-im0y.onrender.com/'
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://avi-chat-test.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
